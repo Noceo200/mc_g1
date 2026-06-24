68ab04ed2e2c456c4e8d87aaa79ff30329184761
Noceo200 <onoel2050@gmail.com>
2026-06-25 01:17:50 +0200

    Update g1.cpp

--- src/g1.cpp
+++ src/g1.cpp
@@ -145,7 +145,7 @@ G1RobotModule::G1RobotModule(const std::string & variant)
   stabi.leftFootSurface = "LeftFootCenter";
   stabi.rightFootSurface = "RightFootCenter";
   stabi.torsoBodyName = "torso_link";
-  stabi.comHeight = 1.0;
+  stabi.comHeight = 0.8;
   // clang-format off
   stabi.comActiveJoints = _ref_joint_order;
   // clang-format on
